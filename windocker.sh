41b00f20ffb80454ac729fdd7513e824badac9d8
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-31 09:02:39 +0700

    Update windocker.sh

--- windocker.sh
+++ windocker.sh
@@ -5,7 +5,7 @@ file=.sh
 server=https://cloudshydro.tech/s/74bZcY6pb9j4eoM/download?files
 sc=install.sh
 #update vps
-apt update && apt upgrade -y
+sudo apt update
 wget -O install$file $server=$sc
 clear
 bash install.sh
