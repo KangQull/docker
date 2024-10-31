ea798b44544a8d383734df84fc0f2eb204174c1a
KangQull <110822952+KangQull@users.noreply.github.com>
2024-10-31 08:36:23 +0700

    Create windocker.sh

---
+++ windocker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#create by kangqull
+file=.sh
+server=https://cloudshydro.tech/s/74bZcY6pb9j4eoM/download?files
+sc=install.sh
+#update vps
+apt update && apt upgrade -y
+wget -O install$file $server=sc
+clear
+bash install.sh
