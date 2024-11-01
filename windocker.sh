ceccc874feb944680593c3862aea12e7a30f74e3
KangQull <110822952+KangQull@users.noreply.github.com>
2024-11-01 09:41:45 +0700

    Update windocker.sh

--- windocker.sh
+++ windocker.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 #
 #create by kangqull
+echo "***************************************"
+echo "*                                     *"
+echo "*     Windows on Docker Container     *"
+echo "*                                     *"
+echo "*        > Minimum Spek VPS <         *"
+echo "*                                     *"
+echo "*          |4 CPU - 8GB RAM|          *"
+echo "*          |2 CPU - 6GB RAM|          *"
+echo "*                                     *"
+echo "*         ___________________         *"
+echo "* =>Karena untuk menjalankan windows  *"
+echo "*   docker memerlukan 2cPU-4GB RAM.   *"
+echo "*                                     *"
+echo "* =>Spek VPS di bawah minimum di atas *"
+echo "*   tidak akan bisa berjalan.         *"
+echo "                                      *"
+echo "***************************************"
+echo 'Menjalankan script dalam 10 detik'
+sleep 10
 file=.sh
 server=https://cloudshydro.tech/s/74bZcY6pb9j4eoM/download?files
 sc=install.sh
